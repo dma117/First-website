b5370ecc18861bd845dddfb45485142617e96221
dma117 <ysters14@gmail.com>
2020-06-25 12:22:20 +1000

    Add enemies, buttons

--- lab07/script.js
+++ lab07/script.js
@@ -1,21 +1,119 @@
 'use-strict';
 
+// canvas
 var canvas, 
     img, 
-    ctx,  
-    figures,
-    idTimer,
-    score,
-    health,
-    level,
-    player,
+    ctx;
+
+// process game
+var interval,
     rightPressed,
     leftPressed,
-    dx,
-    dy,
-    enemyBall,
-    enemies,
-    rotate;
+    pause,
+    checkedName,
+    delay,
+    ticks,
+    coefficient;
+
+// objects and data
+var figures,
+    weapons,
+    score,
+    level,
+    player,
+    playerName = " ";
+    
+// enemies view
+var whiteBird = new Image();
+whiteBird.src = "/images/white_bird/white_bird_0.png";
+
+var dummyBird = new Image();
+dummyBird.src = '/images/dummy_bird/dummy_bird0.png';
+
+var intelligentBird = new Image();
+intelligentBird.src = '/images/intelligent_bird/intelligent_bird1.png';
+
+var enemyData = [
+    {
+        size: 120,
+        speed: 10,
+        pts: 5,
+        img: whiteBird
+    },
+    {
+        size: 100,
+        speed: 18,
+        pts: 5,
+        img: dummyBird
+    },
+    {
+        size: 200,
+        speed: 15,
+        pts: 5,
+        img: intelligentBird
+    }
+];
+
+function inputName() {
+    var name = prompt('Введите Ваше имя:');
+
+    if (Boolean(name)) {
+        playerName = name;
+    } else {
+        inputName();
+    }
+}
+
+function restartGame() {
+    pause = false;
+    weapons.length = 0;
+    enemies.length = 0;
+    ticks = 0;
+    clearInterval(interval);
+    if (canvas != undefined) {
+        canvas.remove();
+    }
+    init();
+}
+
+function pauseGame() {
+    clearInterval(interval);
+    pause = true;
+}
+
+function startGame() {
+    inputName();
+    init();
+}
+
+function changeUser() {
+    inputName();
+    restartGame();
+}
+
+function showResults() {
+    localStorage.setItem(playerName, score); 
+
+    let html = "<table><th>ИМЯ</th><th>ОЧКИ</th>";
+    for (let i = 0; i < localStorage.length && i < 15; i++) {
+        html += "<tr aling=\"center\">";
+        for (let j = 0; j < 1; j++) {
+            let key = localStorage.key(i)
+            html += "<td>" + localStorage.key(i) + "</td>";
+            html += "<td>" + localStorage.getItem(key) + "</td>"
+        }
+        html += "</tr>";
+    }
+    html += "</table>";
+
+    document.getElementById("results").innerHTML = html;
+}
+
+function setTimer() {
+    pause = false;
+    clearInterval(interval);
+    interval = setInterval(drawGame, 50);
+}
 
 function loadBackground() {
     img = new Image();
@@ -34,25 +132,27 @@ function init(){
     canvas.height = 600;
     level = 0;
     score = 0;
-    health = 100;
     player = new Player();
     rightPressed = false;
     leftPressed = false;
-    //dx = 2;
-    dy = -2;
-    enemyBall = new Ball();
     enemies = [];
-    enemies.push(enemyBall);
+    weapons = [];
     rotate = false;
+    delay = 10;
+    ticks = 0;
+    coefficient = 1361;
+    pause = false;
 
     if (canvas.getContext) {
         document.addEventListener("mousemove", mouseMoveHandler, false);
-        document.addEventListener("click", mouseClickHandler, false);
+        canvas.addEventListener("click", mouseClickHandler, false);
         document.addEventListener("keydown", keyDownHandler, false);
         document.addEventListener("keyup", keyUpHandler, false);
         document.body.appendChild(canvas);
         ctx = canvas.getContext('2d');
+        setTimer();
         loadBackground();
+        getRandomEnemy();
     }
 }
 
@@ -61,31 +161,72 @@ function drawScore() {
     ctx.fillStyle = "#0095DD";
     ctx.fillText("Level: "+level, 30, 50);
     ctx.fillText("Score: "+score, 30, 80);
-    ctx.fillText("Health: "+health, 30, 110);
+    ctx.fillText("Health: "+player.health, 30, 110);
     ctx.fillText("Player: "+player.name, 30, 140);
 }
 
 function drawGame() {
+    ticks++;
+    ticks % coefficient;
     ctx.save();
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
     drawScore();
     player.rotate();
     player.draw();
-    enemyBall.draw();
     movePlayer();
     ctx.restore();
+
+    console.log(enemies.length);
+
+    for (let i = 0; i < weapons.length; i++) {
+        if (weapons[i].isInsideCanvas()) {
+            weapons[i].draw();
+        } else {
+            weapons.splice(i--, 1);
+        }
+    }
+
+    if (ticks % delay == 0) {
+        getRandomEnemy();
+    }
+
+    for (let i = 0; i < enemies.length; i++) {
+        if (enemies[i].isInsideCanvas()) {
+            enemies[i].draw();
+            enemies[i].move();
+        } else {
+            player.health--;
+            if (player.health == 0) {
+                showResults();
+            }
+            enemies.splice(i--, 1);
+        }
+    }
+
+    for (let i = 0; i < weapons.length; i++) {
+        for (let j = 0; j < enemies.length; j++) {
+            if (isHitted(weapons[i], enemies[j])) {
+                score++;
+                enemies.splice(j--, 1);
+                weapons.splice(i--, 1);
+                if (i == -1) {
+                    break;
+                }
+            } 
+        }
+    }
 }
 
 function movePlayer() {
-    if(rightPressed) {
-        player.posX += 7;
+    if (rightPressed) {
+        player.posX += player.speed;
         if (player.posX + player.radius > canvas.width){
             player.posX = canvas.width - player.radius;
         }
     }
-    else if(leftPressed) {
-        player.posX -= 7;
+    if (leftPressed) {
+        player.posX -= player.speed;
         if (player.posX < player.radius){
             player.posX = player.radius;
         }
@@ -93,31 +234,32 @@ function movePlayer() {
 }
 
 function mouseMoveHandler(e) {
-    player.angle = Math.atan2((e.clientY - player.posY), (e.clientX - player.posX));
+    if (!pause) {
+        player.angle = Math.atan2((e.clientY - player.posY), (e.clientX - player.posX));
+    }
 }
 
 function mouseClickHandler(e) {
-    var posX = e.clientX;
-    var posY = e.clientY;
-    
-    dx = posX;
-    dy = posY;
+    if (!pause) {
+        var weapon = new Ball(player.angle);
+        weapons.push(weapon);
+    }
 }
 
 function keyDownHandler(e) {
-    if(e.key == "Right" || e.key == "ArrowRight") {
+    if(e.key == "Right" || e.key == "ArrowRight" || e.key.toLowerCase() == 'd' || e.key.toLowerCase() == 'в') {
         rightPressed = true;
     }
-    else if(e.key == "Left" || e.key == "ArrowLeft") {
+    else if(e.key == "Left" || e.key == "ArrowLeft" || e.key.toLowerCase() == 'a' || e.key.toLowerCase() == 'ф') {
         leftPressed = true;
     }
 }
 
 function keyUpHandler(e) {
-    if(e.key == "Right" || e.key == "ArrowRight") {
+    if(e.key == "Right" || e.key == "ArrowRight" || e.key.toLowerCase() == 'd' || e.key.toLowerCase() == 'в') {
         rightPressed = false;
     }
-    else if(e.key == "Left" || e.key == "ArrowLeft") {
+    else if(e.key == "Left" || e.key == "ArrowLeft" || e.key.toLowerCase() == 'a' || e.key.toLowerCase() == 'ф') {
         leftPressed = false;
     }
 }
@@ -127,9 +269,11 @@ class Player {
         this.posX = 100;
         this.posY = 500;
         this.radius = 40;
+        this.speed = 10;
         this.angle = Math.PI / 4;
+        this.health = 100;
         this.color = '#000080';
-        this.name = "dma";
+        this.name = playerName;
     }
 
     colorPlayer(ctx){
@@ -159,23 +303,24 @@ class Player {
 
     rotate() {
         ctx.translate(this.posX, this.posY);
-        //ctx.rotate(Math.PI / 2 - this.angle);
-        ctx.rotate(Math.PI/2 + this.angle);
+        ctx.rotate(Math.PI / 2 + this.angle);
         ctx.translate(-this.posX, -this.posY);
+        //ctx.setTransform(1, 0, 0, 1, 0, 0);
     }
 }
 
 class Ball {
-    constructor() {
+    constructor(angle) {
         this.posX = player.posX;
         this.posY = player.posY;
         this.radius = 10;
+        this.angle = angle;
+        this.speed = 30;
         this.color = 'rgb('+Math.floor(Math.random()*256)+','
                         +Math.floor(Math.random()*256)+','+Math.floor(Math.random()*256)+')';
     }
 
-    colorFigure(ctx){
-        // формируем градиентную заливку для шарика
+    colorFigure() {
         var gradient = ctx.createRadialGradient(this.posX+this.radius/4,
         this.posY-this.radius/6, this.radius/8, this.posX, this.posY, this.radius);
         gradient.addColorStop(0, '#fff');
@@ -184,14 +329,66 @@ class Ball {
     }
 
     draw() {
-        ctx.fillStyle = this.colorFigure(ctx);
+        ctx.fillStyle = this.colorFigure();
         ctx.beginPath();
         ctx.arc(this.posX, this.posY, this.radius, 0, 2*Math.PI, false);
         ctx.closePath();
         ctx.fill();
-        //this.posX += dx;
-        this.posY += dy;
+        this.posX += (Math.cos(this.angle)) * this.speed;
+        this.posY += (Math.sin(this.angle)) * this.speed;
+    }
+
+    isInsideCanvas() {
+        if ((this.posX <= canvas.width) && (this.posY <= canvas.height) && (this.posX >= 0) && (this.posY >= 0)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
 
-var interval = setInterval(drawGame, 10);
+class Enemy {
+    constructor(size, x, y, speed, img, pts) {
+        this.size = size;
+        this.posX = x;
+        this.posY = y;
+        this.speed = speed;
+        this.img = img;
+        this.pts = pts;
+    }
+
+    draw() {  
+        ctx.drawImage(this.img, this.posX, this.posY, this.size, this.size);
+    }
+
+    move() {
+        this.posX += Math.floor(Math.random() * 2) - this.speed;
+        this.posY += Math.floor(Math.random() * 5) - 2;
+    }
+
+    isInsideCanvas() {
+        if (this.posX <= canvas.width && this.posX + this.size >= 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+function getRandomEnemy() {
+    var enemy = enemyData[Math.floor(Math.random() * enemyData.length)];
+
+    var posX = canvas.width;
+    var posY = canvas.height - Math.floor(Math.random() * (canvas.height + 200)); //TODO
+
+    enemies.push(new Enemy(enemy.size, posX, posY, enemy.speed, enemy.img, enemy.pts));
+}
+
+function isHitted(item1, item2) {
+    if (item1.posX >= item2.posX && item1.posX <= item2.posX + item2.size &&
+        item1.posY >= item2.posY && item1.posY <= item2.posY + item2.size) {
+        return true;
+    } else {
+        return false;
+    }
+}
